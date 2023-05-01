d8ac31462886ccdb2739c67a0ecd35abf0937050
willynew <willi@William.com>
2023-05-01 04:41:06 -0500

    debugging

--- provider.tf
+++ provider.tf
@@ -6,4 +6,9 @@ terraform {
     access_key = "AKIARP2QR6XATRRKFV5D"
     secret_key = "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"
 }
+required_providers {
+  aws = {
+    source = "Hashicorp/aws"
+  }
+}
 }

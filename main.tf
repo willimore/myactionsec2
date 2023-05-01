744eb8bad80f9ea2e1e9ccb2871a4e0f1c6461fb
willynew <willi@William.com>
2023-05-01 12:23:33 -0500

    updating

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
+resource "aws_default_vpc" "default_vpc" {
+
+  tags = {
+    Name = "default vpc"
+  }
+}
+
+
 resource "aws_instance" "webec2" {
   ami                    = "ami-007855ac798b5175e"
   instance_type          = "t2.micro"
   key_name               = ""
   monitoring             = true
-  vpc_security_group_ids = [aws_security_group.main.id]
+  vpc_security_group_ids = "[aws_default_vpc.default_vpc.id]"
   subnet_id              = "subnet-febb70df"
 }
- 

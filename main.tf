db4c9c1c9288bbcb51029edd47c5b7e51ec7165b
willynew <willi@William.com>
2023-04-30 21:37:03 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "webec2" {
+  ami                    = ""
+  instance_type          = "t2.micro"
+  key_name               = "purartifact"
+  monitoring             = true
+  vpc_security_group_ids = ["vpc-47dac33d"]
+  subnet_id              = "subnet-febb70df"
+}
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }

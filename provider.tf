f9892f0241d256f7510ff7d093b064bc6cb8ac13
willynew <willi@William.com>
2023-05-01 11:23:04 -0500

    adding

--- provider.tf
+++ provider.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
   backend "s3" {
-    AWS_S3_BUCKET = "${{ secrets.AWS_S3_BUCKET }}"
+    AWS_S3_BUCKET = "secrets.AWS_S3_BUCKET"
     key = "build/terraform.tfstate"
     region = "us-east-1"
-    AWS_ACCESS_KEY_ID = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-    AWS_SECRET_ACCESS_KEY = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
+    AWS_ACCESS_KEY_ID = "secrets.AWS_ACCESS_KEY_ID"
+    AWS_SECRET_ACCESS_KEY = "secrets.AWS_SECRET_ACCESS_KEY"
 }
 
 }

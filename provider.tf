0996196705a174526d29d590481810629e060090
willynew <willi@William.com>
2023-05-01 02:37:07 -0500

    update

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-  bucket = "myfirstwillbucket22"
-  key = "quickmyfirstwillbucket22.tfstate"
-  region = "us-east-1"
+    bucket = "myfirstwillbucket22"
+    key = "quickmyfirstwillbucket22.tfstate"
+    region = "us-east-1"
 }
 }

f58f725370cec0430f6bd0599d071370864b92da
willynew <willi@William.com>
2023-05-01 05:11:16 -0500

    nighly

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "webec2" {
   ami                    = "ami-007855ac798b5175e"
   instance_type          = "t2.micro"
-  key_name               = "putartifact"
+  key_name               = ""
   monitoring             = true
-  vpc_security_group_ids = ["vpc-47dac33d"]
-  subnet_id              = "subnet-febb70df"
+  vpc_security_group_ids = [""]
+  subnet_id              = ""
 }
  
--- provider.tf
+++ provider.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "myfirstwillbucket22"
+    bucket = ""
     key = "build/terraform.tfstate"
     region = "us-east-1"
-    access_key = "AKIARP2QR6XATRRKFV5D"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
 
 }

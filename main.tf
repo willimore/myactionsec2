bdefc74eaf5cc29fd2392a409f6df24a04e2f97e
willynew <willi@William.com>
2023-05-01 01:06:42 -0500

    up

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "webec2" {
   ami                    = ""
   instance_type          = "t2.micro"
-  key_name               = "putartifact"
+  key_name               = ""
   monitoring             = true
-  vpc_security_group_ids = ["vpc-47dac33d"]
-  subnet_id              = "subnet-febb70df"
+  vpc_security_group_ids = [""]
+  subnet_id              = ""
 }
  

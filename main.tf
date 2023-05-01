e4a6c0256b98edcf4f62595f3bcaf7f46900e888
willynew <willi@William.com>
2023-05-01 04:07:07 -0500

    added ami

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "webec2" {
-  ami                    = ""
+  ami                    = "ami-007855ac798b5175e"
   instance_type          = "t2.micro"
   key_name               = "putartifact"
   monitoring             = true

747f1e1b3cafa9ce3e442084af6f39f5dd2d738e
willynew <willi@William.com>
2023-05-01 02:46:21 -0500

    fixing

--- provider.tf
+++ provider.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "myfirstwillbucket22"
     key = "build/terraform.tfstate"
     region = "us-east-1"
+    access_key = "AKIARP2QR6XATRRKFV5D"
+    secret_key = "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"
 }
 }

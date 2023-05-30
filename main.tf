cbfbd08676437dbe05d80361a49f4fe38d59f849
willynew <willi@William.com>
2023-05-30 12:45:55 -0500

    keypair update

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "webec2" {
   ami                    = "ami-007855ac798b5175e"
   instance_type          = "t2.micro"
-  key_name               = ""
+  key_name               = "putartifact"
   monitoring             = true
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 }
